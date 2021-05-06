efc89f9e2f76533df507a2e9a361e42b2af9221a
JS <jseb.minet@gmail.com>
2021-05-06 14:37:49 +0200

    modify unpackWARInstaller

--- resources/unpackWARInstaller.sh
+++ resources/unpackWARInstaller.sh
@@ -4,6 +4,6 @@
 # This file is subject to the license terms contained
 # in the license file that is distributed with this file.
 
-unzip -o -q TIB_js-jrs_*.zip -d .
-cd jasperreports-server-pro-*-bin
-unzip -o -q jasperserver-pro.war -d jasperserver-pro
+unzip -o -q TIB_js-jrs-cp_*.zip -d .
+cd jasperreports-server-cp-*-bin
+unzip -o -q jasperserver.war -d jasperserver
